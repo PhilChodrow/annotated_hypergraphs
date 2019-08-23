cd350bb430967f2726bcfc517b0a1a05c5396699
Andy Mellor <mellor91@hotmail.co.uk>
2019-08-23 11:29:00 +0100

    Refactor. Node-edge incidence is now namedtuple.
    This should help with readability and future work.

--- ahyper/observables.py
+++ ahyper/observables.py
@@ -1,17 +1,18 @@
-from itertools import groupby
+from itertools import groupby, chain
 from collections import Counter
 
 from .utils import normalise_counters
 
-def local_role_density(annotated_hypergraph, include_focus=False):
+def local_role_density(annotated_hypergraph, include_focus=False, absolute_values=False):
     """
     Calculates the density of each role within a 1-step neighbourhood
     of a node, for all nodes.
 
     Input:
-        annotated_hypergraph [AnnotatedHypergraph]: A annotated hypergraph
+        annotated_hypergraph [AnnotatedHypergraph]: An annotated hypergraph.
         include_focus [Bool]: If True, includes the roles of the focal node
-                              in th calculation
+                              in th calculation.
+        absolute_values [Bool]: If True, returns role counts rather than densities.
     Returns:
         role_densities []: An array of dimension (# nodes x # roles) 
                            describing the density of each role.
@@ -19,17 +20,17 @@ def local_role_density(annotated_hypergraph, include_focus=False):
     A = annotated_hypergraph
 
     def get_counts(group):
-        return Counter([x[1] for x in group])
+        return Counter([x.role for x in group])
 
-    by_edge = {eid:get_counts(v) for eid, v in groupby(sorted(A.IL, key=lambda x: x[2], reverse=True), lambda x: x[2])}
-
-    include_focus = False
+    by_edge = {eid:get_counts(v) for eid, v in groupby(sorted(A.IL, key=lambda x: x.eid, reverse=True), lambda x: x.eid)}
 
     densities = {}
-    for node,role,edge,_ in A.IL:
-        densities[node] = densities.get(node, Counter()) + by_edge[edge]
+    for incidence in A.IL:
+        densities[incidence.nid] = densities.get(incidence.nid, Counter()) + by_edge[incidence.eid]
         
         if not include_focus:
+            densities[incidence.nid] = densities.get(incidence.nid, Counter()) - Counter([incidence.role])
+
     keys = set(chain.from_iterable(densities.values()))
     for item in densities.values():
         item.update({key:0 for key in keys if key not in item})
