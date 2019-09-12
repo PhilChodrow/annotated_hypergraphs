1f60a5d0bc03ecd8d70241bd8c63e7de11505906
Andy Mellor <mellor91@hotmail.co.uk>
2019-09-12 13:17:06 +0100

    Further processing features

--- ahyper/ensemble.py
+++ ahyper/ensemble.py
@@ -1,10 +1,34 @@
+import os, json
 from collections import defaultdict
+
+def data_features(annotated_hypergraph,
+                  features):
+    """
+    Calculate features for a single dataset.
+    """
+    A = annotated_hypergraph
+    
+    # Check if we need to calculate the weighted projection.
+    uses_projection = sum([f['acts_on']=='weighted_projection' for f in features.values()]) > 0
+    if uses_projection:
+        W = A.to_weighted_projection(use_networkx=True)
+    
+    feature_store = {}
+    
+    for feature,f in features.items():
+        if f['acts_on'] == 'annotated_hypergraph':
+            feature_store[feature] = f['func'](A, **f['kwargs'])
+        elif f['acts_on'] == 'weighted_projection':
+            feature_store[feature] = f['func'](W, **f['kwargs'])
+            
+    return feature_store
 
 def shuffled_ensemble_features(annotated_hypergraph, 
                                shuffle_fraction, 
                                num_shuffles, 
                                features,
                                burn_fraction=None,
+                               shuffle_algorithm=None,
                                verbose=False):
     """
     Calculate distribution of features for an ensemble of shuffled graphs.
@@ -15,6 +39,8 @@ def shuffled_ensemble_features(annotated_hypergraph,
         num_shuffles (int): The number of iterations
         features (dict): The feature specification (see below)
         burn_fraction (float): The fraction of stubs to be shuffled before data collection
+        shuffle_algorithm (str): The algorithm used to shuffle data. Must be a function of 
+                                 an AnnotatedHypergraph (e.g. A.function())..
         verbose (bool): If True, prints progress. 
 
     Output:
@@ -35,17 +61,22 @@ def shuffled_ensemble_features(annotated_hypergraph,
     # Check if we need to calculate the weighted projection.
     uses_projection = sum([f['acts_on']=='weighted_projection' for f in features.values()]) > 0
     
+    if shuffle_algorithm is None:
+        shuffle = getattr(A,'degeneracy_avoiding_MCMC')
+    else:
+        shuffle = getattr(A, shuffle_algorithm)
+    
     feature_store = defaultdict(dict)
     
     if burn_fraction is not None:
-        A.stub_labeled_MCMC(n_steps=int(shuffle_fraction*num_stubs))
+        shuffle(n_steps=int(shuffle_fraction*num_stubs))
     
     for ix in range(num_shuffles):
         
         # Logging
         if verbose and (ix % (num_shuffles//10)) == 0: print(str(ix)+'%', end='\r', flush=True)
                     
-        A.stub_labeled_MCMC(n_steps=int(shuffle_fraction*num_stubs))
+        shuffle(n_steps=int(shuffle_fraction*num_stubs))
         
         if uses_projection:
             W = A.to_weighted_projection(use_networkx=True)
@@ -57,3 +88,59 @@ def shuffled_ensemble_features(annotated_hypergraph,
                 feature_store[ix][feature] = f['func'](W, **f['kwargs'])
             
     return feature_store
+
+def save_feature_study(annotated_hypergraph,
+                       data_name,
+                       shuffle_fraction, 
+                       num_shuffles, 
+                       features,
+                       burn_fraction=None,
+                       role_preserving=True,
+                       role_destroying=True
+                       ):
+    """
+    """
+    A = annotated_hypergraph
+    
+    # Make directory if doesn't exist.
+    if not os.path.exists(f'./results/{data_name}'):
+        os.makedirs(f'./results/{data_name}')
+    
+    # Make a log of what feature spec was used (although functions are not saved.) 
+    with open(f'./results/{data_name}/feature_log.json', 'w') as file:
+        file.write(json.dumps(features, default=str, indent=4))
+    
+    # Features of pure dataset
+    data = data_features(A,
+                     features)
+    data = pd.Series(data)
+    data.to_csv(f'./results/{data_name}/original.csv')    
+        
+    # Respect roles when performing shuffle
+    if role_preserving:
+        role_preserving_ensemble = shuffled_ensemble_features(A,
+                                        shuffle_fraction,
+                                        num_shuffles,
+                                        features,
+                                        burn_fraction=None,
+                                        shuffle_algorithm='degeneracy_avoiding_MCMC',  
+                                        verbose=False)
+        
+        role_preserving_ensemble = pd.DataFrame(role_preserving_ensemble).T 
+        role_preserving_ensemble.to_csv(f'./results/{data_name}/role_preserving_ensemble.csv', index=False, header=True)
+
+    
+    # How data looks when we do not respect roles.
+    if role_destroying:
+        role_destroying_ensemble = shuffled_ensemble_features(A,
+                                        shuffle_fraction,
+                                        num_shuffles,
+                                        features,
+                                        burn_fraction=None,
+                                        shuffle_algorithm='degeneracy_avoiding_MCMC',  
+                                        verbose=False)
+
+        role_destroying_ensemble = pd.DataFrame(role_destroying_ensemble).T
+        role_destroying_ensemble.to_csv(f'./results/{data_name}/role_destroying_ensemble.csv', index=False, header=True)
+    
+    return None
